3f3710781883afa19dc7d81f1c62b539bec71320
Ben Blum <bblum@andrew.cmu.edu>
2011-11-21 13:05:13 -0500

    add option support

--- Main.hs
+++ Main.hs
@@ -3,12 +3,46 @@
 
 module Main where
 
+import Control.Applicative ((<$>))
 import Control.Monad (when)
 import Language.C
 import Language.C.System.GCC
+import System.Environment (getArgs)
 import System.Exit
+import System.Console.GetOpt
 
 import Check
+
+--
+-- Options & Usage
+--
+
+data Options = Options { help :: Bool, verbose :: Bool }
+defaultOptions = Options { help = False, verbose = False }
+
+desc = [ Option ['h'] ["help"] (NoArg (\o -> o { help = True }))
+         "Show this help text"
+       , Option ['v'] ["verbose"] (NoArg (\o -> o { verbose = True }))
+         "Show info messages in addition to warnings and errors"
+       ]
+
+header = "Atomic All-Nighters - static C code context checking\n" ++
+         "Usage: aaa [OPTION...] SOURCEFILE"
+helptext = usageInfo header desc
+
+parseArgs :: [String] -> Either [String] (Options, String)
+parseArgs a =
+    case getOpt Permute desc a of
+        (opts, [file], []) ->
+            case foldl (flip id) defaultOptions opts of
+                Options { help = True } -> Left [helptext]
+                opts -> Right (opts, file) -- TODO: support mult. files
+        (_, _, []) -> Left [helptext]
+        (_, _, errs) -> Left errs
+
+--
+-- Reading C files
+--
 
 micro_name = "ATOMIC_ALL_NIGHTERS"
 
@@ -22,9 +56,15 @@ parseFile input_file =
 -- TODO: cmdline options
 main :: IO ()
 main =
-    do ast <- parseFile "onion-station.c"
-       print $ pretty ast
-       let msgs = check ast
-       mapM_ putStrLn msgs
-       when (not $ null msgs) $ exitWith $ ExitFailure 1
+    do x <- parseArgs <$> getArgs
+       case x of
+           Left errs ->
+               do mapM_ putStrLn errs
+                  exitWith $ ExitFailure 1
+           Right (opts, file) ->
+               do ast <- parseFile file
+                  print $ pretty ast
+                  let msgs = check ast
+                  mapM_ putStrLn msgs
+                  when (not $ null msgs) $ exitWith $ ExitFailure 1
 
