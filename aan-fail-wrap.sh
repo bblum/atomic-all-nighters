50cb951b9b5604834c937aa7d2794a074aad3f44
Ben Blum <bblum@andrew.cmu.edu>
2013-05-02 16:39:34 -0400

    test wrappers propagate failure

--- aan-fail-wrap.sh
+++ aan-fail-wrap.sh
@@ -2,13 +2,17 @@
 
 DIR=~/atomic-all-nighters
 
+RV=0
+
 for i in $@; do
 	$DIR/bin/aan `cat $DIR/aan-options | tr -d '\n'` $i >/dev/null
 	RES=$?
 	if [ "$RES" = 0 ]; then
 		RES="\033[01;31m$i:\033[80D\033[64CError (shouldn't pass)\033[00m"
+		RV=1
 	else
 		RES="\033[01;32m$i:\033[80D\033[64CSuccess (code $RES)\033[00m"
 	fi
 	echo -e "$RES"
 done
+exit $RV
--- aan-wrap.sh
+++ aan-wrap.sh
@@ -2,6 +2,8 @@
 
 DIR=~/atomic-all-nighters
 
+RV=0
+
 for i in $@; do
 	$DIR/bin/aan `cat $DIR/aan-options | tr -d '\n'` $i
 	RES=$?
@@ -9,6 +11,8 @@ for i in $@; do
 		RES="\033[01;32m$i:\033[80D\033[64CSuccess\033[00m"
 	else
 		RES="\033[01;31m$i:\033[80D\033[64CError $RES\033[00m"
+		RV=1
 	fi
 	echo -e "$RES"
 done
+exit $RV
