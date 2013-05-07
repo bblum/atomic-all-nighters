59fc50b647543ad13b9b0ec12ef005c08bf46da0
Ben Blum <bblum@andrew.cmu.edu>
2013-05-06 20:41:12 -0400

    Update get_language_c.sh for 0.4.2

--- get_language_c.sh
+++ get_language_c.sh
@@ -4,12 +4,12 @@ if [ ! -f Check.hs ]; then
 	exit 1
 fi
 
-wget http://hackage.haskell.org/packages/archive/language-c/0.3.2.1/language-c-0.3.2.1.tar.gz
-tar xvzf language-c-0.3.2.1.tar.gz
-cd language-c-0.3.2.1/src/Language/C/Parser
+wget http://hackage.haskell.org/packages/archive/language-c/0.4.2/language-c-0.4.2.tar.gz
+tar xvzf language-c-0.4.2.tar.gz
+cd language-c-0.4.2/src/Language/C/Parser
 ln -s ../../../../dist/build/Language/C/Parser/Parser.hs .
 ln -s ../../../../dist/build/Language/C/Parser/Lexer.hs .
 cd ../../../../../
-ln -s language-c-0.3.2.1/src/Language/ .
+ln -s language-c-0.4.2/src/Language/ .
 
 echo "You should be good. Type 'make' now."
